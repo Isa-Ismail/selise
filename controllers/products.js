837d5448e8af13924e8a708f59a8eed24f011e90
unknown <fahimismail75.scc@gmail.com>
2022-12-20 13:19:26 +0600

    pagination

--- controllers/products.js
+++ controllers/products.js
@@ -2,8 +2,8 @@ import Products from '../models/Products.js'
 
 export const getAllProd = async ( req, res, next ) => {
     try {
-        const pubs = await Products.find()
-        res.json(pubs)
+        const prods = await Products.find().limit(req.query.perPage).skip(req.query.page*req.query.perPage)
+        res.json(prods)
     } catch (err) {
         next(err)
     }
@@ -11,8 +11,8 @@ export const getAllProd = async ( req, res, next ) => {
 
 export const getProdById = async( req, res, next) => {
     try {
-        const pub = await Products.findById(req.params.id)
-        res.json(pub)
+        const prod = await Products.findById(req.params.id)
+        res.json(prod)
     } catch (err) {
         next(err)
     }
@@ -20,10 +20,29 @@ export const getProdById = async( req, res, next) => {
 
 export const postProd = async ( req, res, next ) => {
     try {
-        const publication = new Products(req.body)
-        await publication.save()
-        res.json({message: "published"})
+        const postProd = new Products(req.body)
+        const newProd = await postProd.save()
+        res.json(newProd)
     } catch (err) {
+        next(err)
+    }
+}
+
+export const updateHotel = async(req, res, next) => {
+    try {
+        const prod = await Products.findByIdAndUpdate(req.params.id, { $set: req.body } )
+        const updatedProd = await prod.save()
+        res.json(updatedProd)
+      } catch (err) {
+        next(err)
+    }
+}
+
+export const deleteHotel = async(req, res, next) => {
+    try {
+        await Products.findByIdAndDelete( req.params.id )
+        res.json({message: 'hotel data deleted'})
+      } catch (err) {
         next(err)
     }
 }
