92a359fcaeff1185d91df6526984ca2f7f028d30
isa-ismail <fahimismail75.scc@gmail.com>
2023-01-22 18:33:31 +0600

    auth

--- models/User.js
+++ models/User.js
@@ -1,10 +1,5 @@
 import mongoose from "mongoose"
 
-const validateEmail = (email) => {
-  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return re.test(email)
-};
-
 const UserSchema = new mongoose.Schema(
   {
     username: {
@@ -15,13 +10,7 @@ const UserSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
-      unique: true,
-      required: "Email address is required",
-      validate: [validateEmail, "Please fill a valid email address"],
-      match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please fill a valid email address",
-      ],
+      unique: true
     },
     country: {
       type: String,
